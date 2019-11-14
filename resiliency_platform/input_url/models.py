8dc41b49799b354ccc6be5c83b5395876048cf31
t-se-surface <tsesurface@gmail.com>
2019-11-14 13:46:02 -0800

    Made edits to the requirement specs, please review

---
+++ resiliency_platform/input_url/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+Class input_url(models.Model)
+    url_text = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': 4, 'cols': 40})) 
+# Create your models here.
