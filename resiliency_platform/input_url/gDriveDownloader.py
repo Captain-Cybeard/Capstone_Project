6f53e3e23300e16251fefbfac47f1172c98e2eee
Ryan <ryanbreitenfeldt@gmail.com>
2020-04-03 17:43:43 -0700

    updated code

--- resiliency_platform/input_url/gDriveDownloader.py
+++ resiliency_platform/input_url/gDriveDownloader.py
@@ -6,6 +6,9 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import flask
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 
 #googleapiclient.discovery.build
 #googleapiclient.discovery.build.files().list()
@@ -20,7 +23,8 @@ class GDriveDownloader():
         self.GDriveDownloader_SCOPES = ['https://www.googleapis.com/auth/drive']
         self.GDriveDownloader_service = service
         self.GDriveDownloader_file_List = None
-        self.GDriveDownloader_json = None
+        self.GDriveDownloader_json = {'files':[]}
+        self.GDriveDownloader_files_to_download = []
 
     def GDriveDownloader_authentication(self):
         # If there are no (valid) credentials available, let the user log in.
@@ -30,6 +34,28 @@ class GDriveDownloader():
             else:
                 flow = InstalledAppFlow.from_client_secrets_file('client_secret_204730000731-c0gs1os80ucalj6mto9c1etmaee70is7.apps.googleusercontent.com.json', self.GDriveDownloader_SCOPES)
                 self.GDriveDownloader_creds = flow.run_local_server(port=0)
+
+    #https://developers.google.com/identity/protocols/oauth2/web-server
+    def GDriveDownloader_redirect_authentication(self):
+        auth_flask = flask()
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file('client_secret_204730000731-c0gs1os80ucalj6mto9c1etmaee70is7.apps.googleusercontent.com.json', self.GDriveDownloader_SCOPES)
+        flow.redirect_uri = 'https://www.google.com'
+        authorization_url, state = flow.authorization_url(access_type='offline',include_granted_scopes='true')
+        auth_flask.redirect(authorization_url)
+        auth_flask.session['state'] = state
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file('client_secret_204730000731-c0gs1os80ucalj6mto9c1etmaee70is7.apps.googleusercontent.com.json',scopes=self.GDriveDownloader_SCOPES,state=state)
+        flow.redirect_uri = flask.url_for('https://www.google.com', _external=True)
+        authorization_response = auth_flask.request.url
+        flow.fetch_token(authorization_response=authorization_response)
+        self.GDriveDownloader_creds = flow.credentials
+        auth_flask.session['credentials'] = {
+            'token': self.GDriveDownloader_creds.token,
+            'refresh_token': self.GDriveDownloader_creds.refresh_token,
+            'token_uri': self.GDriveDownloader_creds.token_uri,
+            'client_id': self.GDriveDownloader_creds.client_id,
+            'client_secret': self.GDriveDownloader_creds.client_secret,
+            'scopes': self.GDriveDownloader_creds.scopes}
+
 
     def GDriveDownloader_save_Token(self):
         with open('token.pickle', 'wb') as token:
@@ -43,27 +69,38 @@ class GDriveDownloader():
     def GDriveDownloader_build_Service(self):
         self.GDriveDownloader_service = build('drive', 'v3', credentials=self.GDriveDownloader_creds)
 
-    def GDriveDownloader__download_File(self,file_Id):
-        request = self.GDriveDownloader_service.files().get_media(fileId=file_id)
-        fh = io.BytesIO()
-        downloader = MediaIoBaseDownload(fh, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print("Download {}%".format(int(status.progress() * 100)))
+    def GDriveDownloader__download_File(self):
+        for file_id in self.GDriveDownloader_files_to_download:
+            request = self.GDriveDownloader_service.files().get_media(fileId=file_id["id"])
+            fh = io.BytesIO()
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print("Download {}%".format(int(status.progress() * 100)))
+            f = open(file_id["name"], 'wb')
+            f.write(fh.getvalue())
 
-    def GDriveDownloader__get_Files(self):
+    def GDriveDownloader_get_Files(self):
         page_token = None
         while True:
-            self.GDriveDownloader_file_List = self.GDriveDownloader_service.files().list(spaces='drive', fields='nextPageToken, items(id, title)', pageToken=page_token).execute()
-            for file in self.GDriveDownloader_file_List.get('items', []):
+            #self.GDriveDownloader_file_List = self.GDriveDownloader_service.files().list(spaces='drive', fields='nextPageToken, items(id, title)', pageToken=page_token).execute()
+            self.GDriveDownloader_file_List = self.GDriveDownloader_service.files().list(q="mimeType != 'application/vnd.google-apps.document' and mimeType != 'application/vnd.google-apps.spreadsheet' and mimeType != 'application/vnd.google-apps.presentation'",spaces='drive', fields='*' , pageToken=page_token).execute()
+            for file in self.GDriveDownloader_file_List["files"]:
                 # Process change
                 #print 'Found file: %s (%s)' % (file.get('title'), file.get('id'))
-                print(file.get('title'))
+                print(file["name"])
+                print(file["id"])
+                print()
+                self.GDriveDownloader_json['files'].append({"name": file["name"],"id": file["id"]})
             page_token = self.GDriveDownloader_file_List.get('nextPageToken', None)
             if page_token is None:
                 break
 
+
+# takes dictionary as input in this formate: {"name": "file name", "id": " file id"}
+    def GDriveDownloader_add_file_to_download(self,fileId):
+        self.GDriveDownloader_files_to_download.append(fileId)
 
 
 
